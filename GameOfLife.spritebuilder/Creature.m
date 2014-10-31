daa1dea9d5c66cda13584eddd1b53dd4837008e0
Aaron Thom <aaronmthom@Aarons-MacBook-Air-2.local>
2014-10-30 22:39:47 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Creature.m
+++ GameOfLife.spritebuilder/Creature.m
@@ -16,13 +16,8 @@
     
     if (self) {
         self.isAlive = NO;
-        - (void)setIsAlive:(BOOL)newState {
-            //when you create an @property as we did in the .h, an instance variable with a leading underscore is automatically created for you
-            _isAlive = newState;
-            
-            // 'visible' is a property of any class that inherits from CCNode. CCSprite is a subclass of CCNode, and Creature is a subclass of CCSprite, so Creatures have a visible property
-            self.visible = _isAlive;
-        }}
+        
+    }
     
     return self;
 }
